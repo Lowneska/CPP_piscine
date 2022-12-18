07917e6aee30c19c833e0f35c4a1c09ca7797b0f
sofia ! <sev.jocks@yahoo.fr>
2022-12-18 23:37:44 +0100

    correc cpp01

--- cpp01/exo03/HumanB.cpp
+++ cpp01/exo03/HumanB.cpp
@@ -3,13 +3,17 @@
 HumanB::HumanB(std::string name)
 {
     this->name = name;
+    this->weapon =  NULL;
 }
 HumanB::~HumanB(){
     std::cout << "HumanB destroyed" << std::endl;
 };
 void HumanB::attack()
 {
-    std::cout << this->name << " attacks with their " << (*weapon).getType() << std::endl;
+    if (weapon != NULL)
+        std::cout << this->name << " attacks with their " << (*weapon).getType() << std::endl;
+    else
+        std::cout << this->name << " has no weapon an cannot attack." << std::endl;
 };
 
 void HumanB::setWeapon(Weapon &club)
